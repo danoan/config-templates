a25c3eba0c8c8dba24a6b6d8d9a82ecc8ef2cf81
DanielMA <danoan2008@gmail.com>
2023-01-07 09:20:46 +0100

    Add python packaging configuration files templates

---
+++ python/project-skeleton/dev/create-venv/create-venv.sh
@@ -0,0 +1,20 @@
+#! /usr/bin/env bash
+
+SCRIPT_PATH="$( cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+# PROJECT_PATH="${SCRIPT_PATH%<main_folder_name>*}<main_folder_name>"
+PROJECT_PATH="$(cd "${SCRIPT_PATH}" && cd ../.. && pwd)"
+
+INPUT_FOLDER="${SCRIPT_PATH}/input"
+OUTPUT_FOLDER="${SCRIPT_PATH}/output"
+mkdir -p "${OUTPUT_FOLDER}"
+
+pushd "${PROJECT_PATH}" > /dev/null
+
+python3.8 -m venv .venv
+source .venv/bin/activate
+
+pip install --upgrade pip
+pip install -e . 
+
+popd > /dev/null
+
---
+++ python/project-skeleton/dev/extract-sdist/extract-sdist.sh
@@ -0,0 +1,25 @@
+#! /usr/bin/env bash
+
+SCRIPT_PATH="$( cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+# PROJECT_PATH="${SCRIPT_PATH%<main_folder_name>*}<main_folder_name>"
+PROJECT_PATH="$(cd "${SCRIPT_PATH}" && cd ../.. && pwd)"
+
+OUTPUT_FOLDER="${SCRIPT_PATH}/output"
+mkdir -p "${OUTPUT_FOLDER}"
+
+pushd "${PROJET_PATH}" > /dev/null
+
+source .venv/bin/activate
+
+DIST_FOLDER="${OUTPUT_FOLDER}/dist"
+mkdir -p "${DIST_FOLDER}"
+
+EXTRACT_FOLDER="${OUTPUT_FOLDER}/extract"
+mkdir -p "${EXTRACT_FOLDER}"
+
+pyproject-build --outdir "${DIST_FOLDER}" .
+
+
+tar -xf "${DIST_FOLDER}/toml-dataclass-0.0.1.tar.gz" -C "${EXTRACT_FOLDER}" 
+
+popd > /dev/null
