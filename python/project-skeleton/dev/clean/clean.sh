9834858a27b1bb4b09a9e92820e518ff3cc3e35e
Daniel Martins Antunes <danoan2008@gmail.com>
2023-09-06 22:49:08 +0200

    [python-skeleton] Setup error handling in scripts
    - setup dev scripts to abort when an error occurs
    - add configuration in pyproject.toml to solve conflict issues in mypy.

--- python/project-skeleton/dev/clean/clean.sh
+++ python/project-skeleton/dev/clean/clean.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 
-SCRIPT_PATH="$( cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+SCRIPT_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # PROJECT_PATH="${SCRIPT_PATH%<main_folder_name>*}<main_folder_name>"
 PROJECT_PATH="$(cd "${SCRIPT_PATH}" && cd ../.. && pwd)"
 
@@ -8,7 +8,10 @@ INPUT_FOLDER="${SCRIPT_PATH}/input"
 OUTPUT_FOLDER="${SCRIPT_PATH}/output"
 mkdir -p "${OUTPUT_FOLDER}"
 
-pushd "${PROJECT_PATH}" > /dev/null
+set -e
+trap "echo 'Aborted!'" err
+
+pushd "${PROJECT_PATH}" >/dev/null
 
 rm -rf ${PROJECT_PATH}/dev/**/output
 rm -rf ${PROJECT_PATH}/build
@@ -17,5 +20,4 @@ rm -rf ${PROJECT_PATH}/**/__pycache__
 rm -rf "${PROJECT_PATH}/.tox"
 rm -rf ${PROJECT_PATH}/**/*.egg-info
 
-popd > /dev/null
-
+popd >/dev/null
--- python/project-skeleton/dev/extract-sdist/extract-sdist.sh
+++ python/project-skeleton/dev/extract-sdist/extract-sdist.sh
@@ -1,13 +1,16 @@
 #! /usr/bin/env bash
 
-SCRIPT_PATH="$( cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+SCRIPT_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # PROJECT_PATH="${SCRIPT_PATH%<main_folder_name>*}<main_folder_name>"
 PROJECT_PATH="$(cd "${SCRIPT_PATH}" && cd ../.. && pwd)"
 
 OUTPUT_FOLDER="${SCRIPT_PATH}/output"
 mkdir -p "${OUTPUT_FOLDER}"
 
-pushd "${PROJET_PATH}" > /dev/null
+set -e
+trap "echo 'Aborted!'" err
+
+pushd "${PROJET_PATH}" >/dev/null
 
 source .venv/bin/activate
 
@@ -19,7 +22,6 @@ mkdir -p "${EXTRACT_FOLDER}"
 
 pyproject-build --outdir "${DIST_FOLDER}" .
 
+tar -xf "${DIST_FOLDER}/toml-dataclass-0.0.1.tar.gz" -C "${EXTRACT_FOLDER}"
 
-tar -xf "${DIST_FOLDER}/toml-dataclass-0.0.1.tar.gz" -C "${EXTRACT_FOLDER}" 
-
-popd > /dev/null
+popd >/dev/null
