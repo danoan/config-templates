d9c514a7a0167fbf24290056d9ac4b3924d62dc2
Daniel Martins Antunes <danoan2008@gmail.com>
2023-09-19 09:44:57 +0200

    [nvim] Add keymaps
    - <leader>fp: Search plugin files.
    - <leader>f1: Search files from home folder.
    - <leader>f2: Search files from Projects folder.
    - <leader>fd: Update work directory and notify.
    - <leader>fg: Switch back to previous working directory.

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -9,56 +9,72 @@ local fn = vim.fn
 local keymap = vim.keymap
 
 -- Editing
-keymap.set('n', '<Leader>w', '<cmd>w<CR>', {desc="Save current buffer"})
-keymap.set('n', 'z-', 'z=1<enter><enter>', {desc="Fix spelling mistakes"})
-keymap.set('v', '<', '<gv', {desc="Align left"})
-keymap.set('v', '>', '>gv', {desc="Align right"})
+keymap.set("n", "<Leader>w", "<cmd>w<CR>", { desc = "Save current buffer" })
+keymap.set("n", "z-", "z=1<enter><enter>", { desc = "Fix spelling mistakes" })
+keymap.set("v", "<", "<gv", { desc = "Align left" })
+keymap.set("v", ">", ">gv", { desc = "Align right" })
 
-keymap.set('v', '<leader>y', '"+y', {desc="Copy to clipboard"})
-keymap.set('n', '<leader>y', '"+y', {desc="Copy to clipboard"})
-keymap.set('n', '<leader>p', 'o<esc>"+gp', {desc="Paste from clipboard"})
+keymap.set("v", "<leader>y", '"+y', { desc = "Copy to clipboard" })
+keymap.set("n", "<leader>y", '"+y', { desc = "Copy to clipboard" })
+keymap.set("n", "<leader>p", 'o<esc>"+gp', { desc = "Paste from clipboard" })
 
 -- Buffer and windows
-keymap.set('n', '<Leader>1', '<cmd>only<CR>', {desc="Only display the current window"})
-keymap.set('n', 'gV', '`[V`]', {desc="Select whole buffer"})
-keymap.set('n', '<leader>gV', '`[V`]"+y <c-o>', {desc="Copy buffer to clipboard"})
+keymap.set("n", "<Leader>1", "<cmd>only<CR>", { desc = "Only display the current window" })
+keymap.set("n", "gV", "`[V`]", { desc = "Select whole buffer" })
+keymap.set("n", "<leader>gV", '`[V`]"+y <c-o>', { desc = "Copy buffer to clipboard" })
 
-keymap.set('n', '<Leader>l', '<cmd>bn<CR>', {desc="Next buffer"})
-keymap.set('n', '<Leader>h', '<cmd>bp<CR>', {desc="Previous buffer"})
-keymap.set('n', '<Leader>q', '<cmd>bp <BAR> bd #<CR>', {desc="Close buffer"})
-keymap.set('n', '<Leader>fd', '<cmd>lcd %:h<CR>', {desc="Matches working directory with current buffer"})
+keymap.set("n", "<Leader>l", "<cmd>bn<CR>", { desc = "Next buffer" })
+keymap.set("n", "<Leader>h", "<cmd>bp<CR>", { desc = "Previous buffer" })
+keymap.set("n", "<Leader>q", "<cmd>bp <BAR> bd #<CR>", { desc = "Close buffer" })
+keymap.set("n", "<Leader>fd", function()
+	vim.cmd("lcd %:h")
+	local new_dir = vim.fn.getcwd()
+	require("notify")(
+		string.format("Working directory has changed to: %s", new_dir),
+		"info",
+		{ title = "Work directory change" }
+	)
+end, { desc = "Matches working directory with current buffer" })
+keymap.set("n", "<Leader>fg", function()
+	vim.cmd("lcd -")
+	local new_dir = vim.fn.getcwd()
+	require("notify")(
+		string.format("Switch to previous directory: %s", new_dir),
+		"info",
+		{ title = "Work directory change" }
+	)
+end, { desc = "Matches working directory with current buffer" })
 
 -- Pane navigation
-keymap.set('n', '<M-h>', '<C-w>h', {desc="Switch to left pane"})
-keymap.set('n', '<M-j>', '<C-w>j', {desc="Switch to right pane"})
-keymap.set('n', '<M-k>', '<C-w>k', {desc="Switch to top pane"})
-keymap.set('n', '<M-l>', '<C-w>l', {desc="Switch to bottom pane"})
+keymap.set("n", "<M-h>", "<C-w>h", { desc = "Switch to left pane" })
+keymap.set("n", "<M-j>", "<C-w>j", { desc = "Switch to right pane" })
+keymap.set("n", "<M-k>", "<C-w>k", { desc = "Switch to top pane" })
+keymap.set("n", "<M-l>", "<C-w>l", { desc = "Switch to bottom pane" })
 
 -- Navigate display lines
-keymap.set('n', 'j', 'gj')
-keymap.set('n', 'k', 'gk')
-keymap.set('v', 'j', 'gj')
-keymap.set('v', 'k', 'gk')
+keymap.set("n", "j", "gj")
+keymap.set("n", "k", "gk")
+keymap.set("v", "j", "gj")
+keymap.set("v", "k", "gk")
 
 -- If using a count to move up or down, ignore display lines
-keymap.set('n', 'k', 'v:count == 0 ? "gk" : "\\<Esc>".v:count."k"', {expr = true})
-keymap.set('n', 'j', 'v:count == 0 ? "gj" : "\\<Esc>".v:count."j"', {expr = true})
+keymap.set("n", "k", 'v:count == 0 ? "gk" : "\\<Esc>".v:count."k"', { expr = true })
+keymap.set("n", "j", 'v:count == 0 ? "gj" : "\\<Esc>".v:count."j"', { expr = true })
 
 -- Line navigation
-keymap.set({'n', 'v'}, 'H', '5h', {desc="Move cursor to the left by 5"})
-keymap.set({'n', 'v'}, 'J', '5j', {desc="Move cursor to the bottom by 5"})
-keymap.set({'n', 'v'}, 'K', '5k', {desc="Move cursor to the top by 5", remap = true})
-keymap.set({'n', 'v'}, 'L', '5l', {desc="Move cursor to the right by 5"})
+keymap.set({ "n", "v" }, "H", "5h", { desc = "Move cursor to the left by 5" })
+keymap.set({ "n", "v" }, "J", "5j", { desc = "Move cursor to the bottom by 5" })
+keymap.set({ "n", "v" }, "K", "5k", { desc = "Move cursor to the top by 5", remap = true })
+keymap.set({ "n", "v" }, "L", "5l", { desc = "Move cursor to the right by 5" })
 
 -- Page navigation
-keymap.set('n', '<c-e>', '7<c-e>', {desc="Smooth page down"})
-keymap.set('n', '<c-y>', '7<c-y>', {desc="Smooth page up"})
-keymap.set('v', '<c-e>', '7<c-e>', {desc="Smooth page down"})
-keymap.set('v', '<c-y>', '7<c-y>', {desc="Smooth page up"})
+keymap.set("n", "<c-e>", "7<c-e>", { desc = "Smooth page down" })
+keymap.set("n", "<c-y>", "7<c-y>", { desc = "Smooth page up" })
+keymap.set("v", "<c-e>", "7<c-e>", { desc = "Smooth page down" })
+keymap.set("v", "<c-y>", "7<c-y>", { desc = "Smooth page up" })
 
-
-keymap.set('n', '<Leader>a', '<cmd>noh<CR>', {desc="Turn off term highlight"})
+keymap.set("n", "<Leader>a", "<cmd>noh<CR>", { desc = "Turn off term highlight" })
 
 -- Daniel's Keys
-keymap.set('n','<C-D>c', '<cmd>let @" = expand("%:p")<cr>', {desc="Copy current file path"})
-
+keymap.set("n", "<C-D>c", '<cmd>let @" = expand("%:p")<cr>', { desc = "Copy current file path" })
+keymap.set("n", "<C-D>i", "<cmd>e ~/.config/nvim/init.lua<cr>", { desc = "Open main init.lua" })
--- nvim/lua/plugins/overrides.lua
+++ nvim/lua/plugins/overrides.lua
@@ -8,4 +8,31 @@ return {
 			keys[#keys + 1] = { "oo", vim.lsp.buf.hover }
 		end,
 	},
+	{
+		"nvim-telescope/telescope.nvim",
+		keys = {
+			-- add a keymap to browse plugin files
+			{
+				"<leader>fp",
+				function()
+					require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root })
+				end,
+				desc = "Find Plugin File",
+			},
+			{
+				"<leader>f1",
+				function()
+					require("telescope.builtin").find_files({ cwd = "~", prompt = "Home" })
+				end,
+				desc = "Find files from the home directory",
+			},
+			{
+				"<leader>f2",
+				function()
+					require("telescope.builtin").find_files({ cwd = "~/Projects", prompt = "Projects" })
+				end,
+				desc = "Find files from the ~/Projects",
+			},
+		},
+	},
 }
