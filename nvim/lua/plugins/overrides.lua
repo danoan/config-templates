399c4d884ba17a525e92b6c66fc84318565cdab3
Daniel Martins Antunes <dmartins@soundhound.com>
2026-01-21 15:28:38 +0100

    Update to nvim-0.11.5

--- nvim/lua/plugins/overrides.lua
+++ nvim/lua/plugins/overrides.lua
@@ -1,38 +1,43 @@
 -- This file overrides options of default plugins in lazy.nvim
 return {
-	{
-		"neovim/nvim-lspconfig",
-		init = function()
-			local keys = require("lazyvim.plugins.lsp.keymaps").get()
-			keys[#keys + 1] = { "K", false }
-			keys[#keys + 1] = { "oo", vim.lsp.buf.hover }
-		end,
-	},
-	{
-		"nvim-telescope/telescope.nvim",
-		keys = {
-			-- add a keymap to browse plugin files
-			{
-				"<leader>fp",
-				function()
-					require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root })
-				end,
-				desc = "Find Plugin File",
-			},
-			{
-				"<leader>f1",
-				function()
-					require("telescope.builtin").find_files({ cwd = "~", prompt = "Home" })
-				end,
-				desc = "Find files from the home directory",
-			},
-			{
-				"<leader>f2",
-				function()
-					require("telescope.builtin").find_files({ cwd = "~/Projects", prompt = "Projects" })
-				end,
-				desc = "Find files from the ~/Projects",
-			},
-		},
-	},
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        ["*"] = {
+          keys = {
+            { "K", false },
+            { "oo", vim.lsp.buf.hover },
+          },
+        },
+      },
+    },
+  },
+  {
+    "nvim-telescope/telescope.nvim",
+    keys = {
+      -- add a keymap to browse plugin files
+      {
+        "<leader>fp",
+        function()
+          require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root })
+        end,
+        desc = "Find Plugin File",
+      },
+      {
+        "<leader>f1",
+        function()
+          require("telescope.builtin").find_files({ cwd = "~", prompt = "Home" })
+        end,
+        desc = "Find files from the home directory",
+      },
+      {
+        "<leader>f2",
+        function()
+          require("telescope.builtin").find_files({ cwd = "~/Projects", prompt = "Projects" })
+        end,
+        desc = "Find files from the ~/Projects",
+      },
+    },
+  },
 }
